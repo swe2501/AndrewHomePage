15b48815ccbd41f118189adc9a2ca9ce053d4530
Andrew <swe250165@gmail.com>
2021-06-14 11:01:01 +0800

    增加router連結

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { LandingComponent } from './examples/landing/landing.component';
 import { LoginComponent } from './examples/login/login.component';
 import { ProfileComponent } from './examples/profile/profile.component';
@@ -14,14 +15,15 @@ import { WorksSharingComponent } from './examples/worksSharing/worksSharing.comp
 
 const routes: Routes =[
     { path: '', redirectTo: 'index', pathMatch: 'full' },
-    { path: 'index',                component: ComponentsComponent },
+    { path: 'index',                component: ComponentsComponent, data: {animation: 'HomePage'} },
     { path: 'nucleoicons',          component: NucleoiconsComponent },
-    { path: 'examples/landing',     component: LandingComponent },
-    { path: 'examples/login',       component: LoginComponent },
+    { path: 'examples/landing',     component: LandingComponent ,data: {animation: 'LandingPage'}},
+    { path: 'examples/login',       component: LoginComponent ,data: {animation: 'LoginPage'}},
     { path: 'examples/profile',     component: ProfileComponent },
-    { path: 'examples/team',        component: TeamComponent },
-    { path: 'examples/worksSharing',component: WorksSharingComponent },
+    { path: 'examples/team',        component: TeamComponent ,data: {animation: 'TeamPage'}},
+    { path: 'examples/worksSharing',component: WorksSharingComponent ,data: {animation: 'WorksSharing'}},
     { path: 'examples',             component: ExamplesComponent },
+    { path: 'projects',             component: ProjectsComponent },
 ];
 
 @NgModule({
